388bc238b01367b1a84c882c92c3fbe2c8c5087c
João Paulo <jpsampaiosenac@gmail.com>
2026-06-26 10:01:16 -0300

    Att 2 projeto v2

--- Projeto_Dia_26-06/projeto-versao2.py
+++ Projeto_Dia_26-06/projeto-versao2.py
@@ -1,29 +1,50 @@
-#Início
-#Ideias de Variáveis: num1,Win,cont,num2
+# Início
+# Ideias de Variáveis: num1,Win,cont,num2
 cont = 0
 cont_tentativas = 0
 pontos = 0
 pontos2 = 0
 diferenca = 0
-pergunta = str##########################CUIDADOOOO AQUI, OLHA DEPOIS
+pergunta = str  # CUIDADOOOO AQUI, OLHA DEPOIS
 ultima_resposta = False
 cont_respostas_corretas = 0
-#Usuário informa o nome
+
+# Usuário informa o nome
 nome = str(input("Qual seu nome? "))
 print(f"Seja bem vindo {nome} ao São João da Diversidade")
-#Explicação das regras
+# Explicação das regras
 print("O jogo de Verdadeiro ou Falso começou. O jogo tem 12 peguntas objetivas, sendo 4 fáceis, 4 Médias e 4 Difíceis.")
-#Perguntas(Verdadeiro ou Falso)
+# Perguntas(Verdadeiro ou Falso)
 print("")
 
-#Sistema Verifica a resposta
-if pergunta == True:
-    pontos = pontos + 1
+# Sistema Verifica a resposta
 
-#Mostra explicação da resposta
+while True:
 
-#Atualiza pontuação
+    print("O Sao João é a festa da colheita do milho? 1 - Sim 2 - Não")
+    resposta = input()
+    if resposta == "sim" or resposta == "1":
+        pontos = pontos + 1
 
-#Resultado Final
+    print("O são João é comemorado tradicionalmente no mes de junho? 1 - Sim 2 - Não")
+    resposta = input()
+    if resposta == "sim" or resposta == "1":
+        pontos = pontos + 1
+    
+    
+    
+    
+    
+    
+    print("Parabens sua pontuação foi de: ", pontos)
+    jogar_novamente = input("Quer ir de novo? ")
+    if jogar_novamente == "Não" or jogar_novamente == "não" or jogar_novamente == "nao" or jogar_novamente == "Nao":
+        break
 
-#Fim (Parabéns Usuário)
+# Mostra explicação da resposta
+
+# Atualiza pontuação
+
+# Resultado Final
+
+# Fim (Parabéns Usuário)
