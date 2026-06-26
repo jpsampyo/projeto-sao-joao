907d5afa106efbd298cb72a5bc706790fefe1118
João Paulo <jpsampaiosenac@gmail.com>
2026-06-26 09:37:51 -0300

    versao 2, aprimoramento do jogo

---
+++ Projeto_Dia_26-06/projeto-versao2.py
@@ -0,0 +1,29 @@
+#Início
+#Ideias de Variáveis: num1,Win,cont,num2
+cont = 0
+cont_tentativas = 0
+pontos = 0
+pontos2 = 0
+diferenca = 0
+pergunta = str##########################CUIDADOOOO AQUI, OLHA DEPOIS
+ultima_resposta = False
+cont_respostas_corretas = 0
+#Usuário informa o nome
+nome = str(input("Qual seu nome? "))
+print(f"Seja bem vindo {nome} ao São João da Diversidade")
+#Explicação das regras
+print("O jogo de Verdadeiro ou Falso começou. O jogo tem 12 peguntas objetivas, sendo 4 fáceis, 4 Médias e 4 Difíceis.")
+#Perguntas(Verdadeiro ou Falso)
+print("")
+
+#Sistema Verifica a resposta
+if pergunta == True:
+    pontos = pontos + 1
+
+#Mostra explicação da resposta
+
+#Atualiza pontuação
+
+#Resultado Final
+
+#Fim (Parabéns Usuário)
